25d81a6627ba300a9fe7cfb11f6e5435e8fb604f
Mundo <18106524342@189.cn>
2025-04-25 15:20:49 +0800

    更新wresp，新增流式接口包装

--- wresp/stdresp.go
+++ wresp/stdresp.go
@@ -13,15 +13,16 @@ type middlewareWrapper func(c *gin.Context) error
 
 type fileDownloadWrapper func(c *gin.Context) (string, error)
 
+type streamHandlerWrapper func(c *gin.Context) error
+
 type Server struct {
 	Router *gin.Engine
 }
 
 type response struct {
-	Code      int         `json:"code"`
-	Message   string      `json:"message"`
-	PrintInfo string      `json:"printInfo,omitempty"` // 返回到前端，只要有该字段，弹窗展示给用户
-	Data      interface{} `json:"data"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"` // 如果code值不为0，前端展示message内容给用户
+	Data    interface{} `json:"data"`
 }
 
 type errorCode struct {
@@ -35,7 +36,7 @@ func (e *errorCode) Error() string {
 }
 
 func NewErrorCode(code int, message string) error {
-	return NewErrorCodeWithStatus(code, message, http.StatusInternalServerError) // 默认设置状态码500
+	return NewErrorCodeWithStatus(code, message, http.StatusInternalServerError) // 默认设置HTTP状态码500
 }
 
 func NewErrorCodeWithStatus(code int, message string, httpStatus int) error {
@@ -46,7 +47,6 @@ func NewErrorCodeWithStatus(code int, message string, httpStatus int) error {
 	}
 }
 
-// IsErrorCode 用于判断给定的错误是否为定义的错误码（即是否为*errorCode类型）
 func IsErrorCode(target error) bool {
 	_, ok := target.(*errorCode)
 	return ok
@@ -57,19 +57,32 @@ func handleErrorResponse(c *gin.Context, err error, abort bool) {
 	httpStatus := http.StatusInternalServerError
 	if e, ok := err.(*errorCode); ok {
 		resp.Code = e.code
-		resp.Message = e.message
-		resp.PrintInfo = e.Error()
+		resp.Message = e.Error() // 将Error()方法返回的格式字符串写入到message
 		httpStatus = e.httpStatus
 	} else {
 		resp.Code = -1
-		resp.Message = err.Error() // 如果断言失败，e将会是nil，如果使用e.Error()会造成空指针
-		resp.PrintInfo = "内部错误，请联系平台工作人员"
+		resp.Message = "内部错误，请联系平台工作人员"
 	}
 	if abort {
 		c.AbortWithStatusJSON(httpStatus, resp)
 	} else {
 		c.JSON(httpStatus, resp)
 	}
+}
+
+func writeStreamError(c *gin.Context, err error) {
+	if !c.Writer.Written() {
+		handleErrorResponse(c, err, false)
+		return
+	}
+	var message string
+	if e, ok := err.(*errorCode); ok {
+		message = e.Error()
+	} else {
+		message = "内部错误，请联系平台工作人员"
+	}
+	_, _ = fmt.Fprintf(c.Writer, "%s\n", message) // 网络写入失败可在工具层忽略
+	c.Writer.Flush()
 }
 
 func (s *Server) WrapHandler(wrapper handlerWrapper) gin.HandlerFunc {
@@ -111,5 +124,18 @@ func (s *Server) WrapFileDownload(wrapper fileDownloadWrapper, download bool) gi
 			c.Header("Content-Disposition", fmt.Sprintf("attachment; fileName=%s", fileName))
 		}
 		c.File(filePath)
+	}
+}
+
+func (s *Server) WrapStreamHandler(wrapper streamHandlerWrapper) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "text/event-stream")
+		c.Writer.Header().Set("Cache-Control", "no-cache")
+		c.Writer.Header().Set("Connection", "keep-alive")
+		c.Writer.Flush()
+		err := wrapper(c)
+		if err != nil {
+			writeStreamError(c, err)
+		}
 	}
 }

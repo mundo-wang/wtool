8d5f355c9feeb6f764ef335f5166a07019431ca5
Mundo <18106524342@189.cn>
2025-05-26 14:00:19 +0800

    更新whttp，采用泛型方式对工具进行改造

--- whttp/client.go
+++ whttp/client.go
@@ -6,34 +6,38 @@ import (
 	"time"
 )
 
-// HttpClient 定义了httpClient所有需要暴露的方法
-type HttpClient interface {
-	WithJsonBody(body interface{}) HttpClient
-	WithPathParam(args ...string) HttpClient
-	WithQueryParam(key, value string) HttpClient
-	WithQueryParamByMap(params map[string]string) HttpClient
-	WithQueryParamByStruct(params interface{}) HttpClient
-	WithHeader(key, value string) HttpClient
-	WithHeaderByMap(headers map[string]string) HttpClient
-	Send() ([]byte, error)
+// 定义了httpClient所有需要暴露的方法
+type HttpClient[T any] interface {
+	WithJsonBody(body interface{}) HttpClient[T]
+	WithPathParam(args ...string) HttpClient[T]
+	WithQueryParam(key, value string) HttpClient[T]
+	WithQueryParamByMap(params map[string]string) HttpClient[T]
+	WithQueryParamByStruct(params interface{}) HttpClient[T]
+	WithHeader(key, value string) HttpClient[T]
+	WithHeaderByMap(headers map[string]string) HttpClient[T]
+	Send() HttpClient[T]
+	Error() error
+	GetResp() *T
 	GetRespHeader(key string) string
 	GetRespHeaderMulti(key string) []string
 }
 
-// httpClient HttpClient接口的实现结构体，私有
-type httpClient struct {
+// HttpClient接口的实现结构体，私有
+type httpClient[T any] struct {
 	baseURL     string
 	method      string
 	queryParams url.Values
 	jsonBody    []byte
 	headers     map[string]string
 	client      *http.Client
-	respHeaders http.Header
 	err         error
+	respHeaders http.Header
+	respBytes   []byte
+	resp        *T
 }
 
-// NewHttpClient 创建一个新的httpClient实例
-func NewHttpClient(baseURL, method string, timeout time.Duration) HttpClient {
+// 泛型类型参数T表示返回的数据结构类型
+func NewHttpClient[T any](baseURL, method string, timeout time.Duration) HttpClient[T] {
 	transport := &http.Transport{
 		MaxIdleConns:        10,
 		MaxIdleConnsPerHost: 10,
@@ -42,10 +46,10 @@ func NewHttpClient(baseURL, method string, timeout time.Duration) HttpClient {
 	client := &http.Client{
 		Transport: transport,
 	}
-	if timeout > 0 {
+	if timeout > 0 { // timeout = 0，代表不设置超时时间
 		client.Timeout = timeout
 	}
-	return &httpClient{
+	return &httpClient[T]{
 		baseURL:     baseURL,
 		method:      method,
 		queryParams: url.Values{},
--- whttp/methods.go
+++ whttp/methods.go
@@ -13,8 +13,8 @@ import (
 	"github.com/mundo-wang/wtool/wlog" // 替换为指定的wlog路径
 )
 
-// WithJsonBody 设置JSON请求体
-func (cli *httpClient) WithJsonBody(body interface{}) HttpClient {
+// 设置JSON请求体
+func (cli *httpClient[T]) WithJsonBody(body interface{}) HttpClient[T] {
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
 		wlog.Error("call json.Marshal failed").Err(err).Field("url", cli.baseURL).Field("method", cli.method).Log()
@@ -26,8 +26,8 @@ func (cli *httpClient) WithJsonBody(body interface{}) HttpClient {
 	return cli
 }
 
-// WithPathParam 设置路径参数
-func (cli *httpClient) WithPathParam(args ...string) HttpClient {
+// 设置路径参数
+func (cli *httpClient[T]) WithPathParam(args ...string) HttpClient[T] {
 	u, err := url.Parse(cli.baseURL)
 	if err != nil {
 		wlog.Error("call url.Parse failed").Err(err).Field("url", cli.baseURL).Field("method", cli.method).Log()
@@ -40,24 +40,24 @@ func (cli *httpClient) WithPathParam(args ...string) HttpClient {
 	return cli
 }
 
-// WithQueryParam 设置单个查询参数
-func (cli *httpClient) WithQueryParam(key, value string) HttpClient {
+// 设置单个查询参数
+func (cli *httpClient[T]) WithQueryParam(key, value string) HttpClient[T] {
 	if value != "" {
 		cli.queryParams.Set(key, value)
 	}
 	return cli
 }
 
-// WithQueryParamByMap 设置查询参数（通过map）
-func (cli *httpClient) WithQueryParamByMap(params map[string]string) HttpClient {
+// 通过map设置多个查询参数
+func (cli *httpClient[T]) WithQueryParamByMap(params map[string]string) HttpClient[T] {
 	for key, value := range params {
 		cli.WithQueryParam(key, value)
 	}
 	return cli
 }
 
-// WithQueryParamByStruct 设置查询参数（通过结构体）
-func (cli *httpClient) WithQueryParamByStruct(params interface{}) HttpClient {
+// 通过结构体对象，设置多个查询参数
+func (cli *httpClient[T]) WithQueryParamByStruct(params interface{}) HttpClient[T] {
 	queryParams, err := query.Values(params)
 	if err != nil {
 		wlog.Error("call query.Values failed").Err(err).Field("url", cli.baseURL).Field("method", cli.method).Log()
@@ -72,27 +72,24 @@ func (cli *httpClient) WithQueryParamByStruct(params interface{}) HttpClient {
 	return cli
 }
 
-// WithHeader 设置请求头
-func (cli *httpClient) WithHeader(key, value string) HttpClient {
+// 设置请求头
+func (cli *httpClient[T]) WithHeader(key, value string) HttpClient[T] {
 	if value != "" {
 		cli.headers[key] = value
 	}
 	return cli
 }
 
-// WithHeaderByMap 设置请求头（通过map）
-func (cli *httpClient) WithHeaderByMap(headers map[string]string) HttpClient {
+// 通过map设置多个请求头
+func (cli *httpClient[T]) WithHeaderByMap(headers map[string]string) HttpClient[T] {
 	for key, value := range headers {
 		cli.WithHeader(key, value)
 	}
 	return cli
 }
 
-// Send 发送请求并返回响应
-func (cli *httpClient) Send() ([]byte, error) {
-	if cli.err != nil {
-		return nil, cli.err
-	}
+// 发送请求并封装响应
+func (cli *httpClient[T]) Send() HttpClient[T] {
 	var fullURL string
 	if len(cli.queryParams) > 0 {
 		fullURL = fmt.Sprintf("%s?%s", cli.baseURL, cli.queryParams.Encode())
@@ -106,7 +103,8 @@ func (cli *httpClient) Send() ([]byte, error) {
 	httpReq, err := http.NewRequest(cli.method, fullURL, body)
 	if err != nil {
 		wlog.Error("call http.NewRequest failed").Err(err).Field("url", fullURL).Field("method", cli.method).Log()
-		return nil, err
+		cli.err = err
+		return cli
 	}
 	for key, value := range cli.headers {
 		httpReq.Header.Set(key, value)
@@ -114,40 +112,67 @@ func (cli *httpClient) Send() ([]byte, error) {
 	httpResp, err := cli.client.Do(httpReq)
 	if err != nil {
 		wlog.Error("call cli.client.Do failed").Err(err).Field("url", fullURL).Field("method", cli.method).Log()
-		return nil, err
+		cli.err = err
+		return cli
 	}
 	defer httpResp.Body.Close()
 	cli.respHeaders = httpResp.Header
 	respBytes, err := io.ReadAll(httpResp.Body)
 	if err != nil {
 		wlog.Error("call io.ReadAll failed").Err(err).Field("url", fullURL).Field("method", cli.method).Log()
-		return nil, err
+		cli.err = err
+		return cli
 	}
 	// 由于一些HTTP接口返回的成功状态码不一定为200，所以这里判断只要是2开头的状态码，均视为请求成功
 	if httpResp.StatusCode >= 200 && httpResp.StatusCode < 300 {
-		return respBytes, nil
+		cli.respBytes = respBytes
+		resp := new(T)
+		err = json.Unmarshal(respBytes, resp)
+		if err != nil {
+			wlog.Error("call json.Unmarshal failed").Err(err).Field("url", fullURL).Field("method", cli.method).Log()
+			cli.err = err
+		}
+		cli.resp = resp
+		return cli
 	}
 	var errorResp map[string]interface{}
 	err = json.Unmarshal(respBytes, &errorResp)
 	if err != nil {
 		wlog.Error("call json.Unmarshal failed").Err(err).Field("url", fullURL).
 			Field("method", cli.method).Field("statusCode", httpResp.StatusCode).Log()
-		return nil, err
+		cli.err = err
+		return cli
 	}
 	err = fmt.Errorf("status code not 200, is %d", httpResp.StatusCode)
 	wlog.Error("call cli.client.Do failed").Err(err).Field("url", fullURL).
 		Field("method", cli.method).Field("errorResp", errorResp).Log()
-	return nil, err
+	cli.err = err
+	return cli
+}
+
+// 检查发送请求过程中是否有报错
+func (cli *httpClient[T]) Error() error {
+	return cli.err
+}
+
+// 返回响应体的字节数组
+func (cli *httpClient[T]) GetRespBytes() []byte {
+	return cli.respBytes
+}
+
+// 返回响应体反序列化的对象
+func (cli *httpClient[T]) GetResp() *T {
+	return cli.resp
 }
 
 // GetRespHeader 获取指定key关联的第一个值，如果无关联，返回空字符串
-func (cli *httpClient) GetRespHeader(key string) string {
+func (cli *httpClient[T]) GetRespHeader(key string) string {
 	value := cli.respHeaders.Get(key)
 	return value
 }
 
 // GetRespHeaderMulti 获取指定key关联的所有值，如果无关联，返回空切片
-func (cli *httpClient) GetRespHeaderMulti(key string) []string {
+func (cli *httpClient[T]) GetRespHeaderMulti(key string) []string {
 	values := cli.respHeaders.Values(key)
 	return values
 }

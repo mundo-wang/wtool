59d621029b754c975a5aed08030ca44ee3db9842
Mundo <18106524342@189.cn>
2025-06-18 09:24:12 +0800

    更新wlog函数可见性内容

--- wlog/entry.go
+++ wlog/entry.go
@@ -21,7 +21,7 @@ type loggerEntry struct {
 	skip    int
 }
 
-func NewLoggerEntry(level zapcore.Level, message string) LoggerEntry {
+func newLoggerEntry(level zapcore.Level, message string) LoggerEntry {
 	return &loggerEntry{
 		level:   level,
 		logger:  logger,
@@ -33,8 +33,8 @@ func NewLoggerEntry(level zapcore.Level, message string) LoggerEntry {
 // 获取调用日志的函数或方法全名的最后一部分，一般来说是最后一个斜杠后的部分
 // 对于函数，其全名为module/paths/pkg.FuncName(其中 paths 是从模块根目录到包的相对路径)，处理后返回的是pkg.FuncName
 // 对于方法，其全名同理于函数，处理后返回的是pkg.(*Type).MethodName或者pkg.Type.MethodName
-func CallerName(skip int) string {
-	pc, _, _, ok := runtime.Caller(skip + 1) // 这里需要+1，是因为要先跳转到调用CallerName的Log方法
+func callerName(skip int) string {
+	pc, _, _, ok := runtime.Caller(skip + 1) // 这里需要+1，是因为要先跳转到调用callerName的Log方法
 	if ok {
 		fullName := runtime.FuncForPC(pc).Name()
 		baseName := path.Base(fullName)
@@ -54,12 +54,17 @@ func (l *loggerEntry) Err(err error) LoggerEntry {
 	return l
 }
 
+// 表示跳过调用栈中的若干层，用于控制日志中显示的调用位置。
+// 例如：函数A调用函数B，函数B调用函数C，函数C中调用Error方法。
+// 默认情况下跳过1层，日志将显示函数C中调用Error的代码位置；
+// 若设置为跳过2层，则显示函数B中调用函数C的位置；
+// 若设置为跳过3层，则显示函数A中调用函数B的位置。
 func (l *loggerEntry) Skip(skip int) LoggerEntry {
 	l.skip = skip
 	return l
 }
 
 func (l *loggerEntry) Log() {
-	l.logger.With(zap.String("caller", CallerName(l.skip))).
+	l.logger.With(zap.String("caller", callerName(l.skip))).
 		WithOptions(zap.AddCallerSkip(l.skip)).Check(l.level, l.message).Write()
 }
--- wlog/level.go
+++ wlog/level.go
@@ -6,55 +6,55 @@ import (
 )
 
 func Debug(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.DebugLevel, message)
+	return newLoggerEntry(zapcore.DebugLevel, message)
 }
 
 func Info(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.InfoLevel, message)
+	return newLoggerEntry(zapcore.InfoLevel, message)
 }
 
 func Warn(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.WarnLevel, message)
+	return newLoggerEntry(zapcore.WarnLevel, message)
 }
 
 func Error(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.ErrorLevel, message)
+	return newLoggerEntry(zapcore.ErrorLevel, message)
 }
 
 func Fatal(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.FatalLevel, message)
+	return newLoggerEntry(zapcore.FatalLevel, message)
 }
 
 func Panic(message string) LoggerEntry {
-	return NewLoggerEntry(zapcore.PanicLevel, message)
+	return newLoggerEntry(zapcore.PanicLevel, message)
 }
 
 func Debugf(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.DebugLevel, message)
+	return newLoggerEntry(zapcore.DebugLevel, message)
 }
 
 func Infof(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.InfoLevel, message)
+	return newLoggerEntry(zapcore.InfoLevel, message)
 }
 
 func Warnf(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.WarnLevel, message)
+	return newLoggerEntry(zapcore.WarnLevel, message)
 }
 
 func Errorf(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.ErrorLevel, message)
+	return newLoggerEntry(zapcore.ErrorLevel, message)
 }
 
 func Fatalf(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.FatalLevel, message)
+	return newLoggerEntry(zapcore.FatalLevel, message)
 }
 
 func Panicf(format string, args ...interface{}) LoggerEntry {
 	message := fmt.Sprintf(format, args...)
-	return NewLoggerEntry(zapcore.PanicLevel, message)
+	return newLoggerEntry(zapcore.PanicLevel, message)
 }
